7b4fc239ca7fb3c9edbf9a32267d2e4aa8c86607
unknown <chavarriacarlos18@gmail.com>
2019-08-04 00:08:03 -0700

    fix

--- 01 - JavaScript Drum Kit/index.js
+++ 01 - JavaScript Drum Kit/index.js
@@ -22,16 +22,7 @@ keys.forEach(key => {
 });
 
 // only display keyboard when on small device (assume mobile)
-
-if (
-  navigator.userAgent.match(/Android/i) ||
-  navigator.userAgent.match(/webOS/i) ||
-  navigator.userAgent.match(/iPhone/i) ||
-  navigator.userAgent.match(/iPad/i) ||
-  navigator.userAgent.match(/iPod/i) ||
-  navigator.userAgent.match(/BlackBerry/i) ||
-  navigator.userAgent.match(/Windows Phone/i)
-) {
+if (window.outerWidth <= 1024) {
   document.getElementById(
     'change'
   ).innerHTML = `<h1 style='font-size: 50px; color: white;'>Must use a device with a keyboard.</h1>`;
