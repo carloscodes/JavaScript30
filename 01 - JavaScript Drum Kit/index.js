a1ae9488dcf203f838065e26e2d196b5f97ee12a
unknown <chavarriacarlos18@gmail.com>
2019-08-03 23:59:34 -0700

    mobile detection

--- 01 - JavaScript Drum Kit/index.js
+++ 01 - JavaScript Drum Kit/index.js
@@ -22,6 +22,17 @@ keys.forEach(key => {
 });
 
 // only display keyboard when on small device (assume mobile)
-if (window.outerWidth <= 1024) {
-  document.write('Please use a device with a keyboard..');
+
+if (
+  navigator.userAgent.match(/Android/i) ||
+  navigator.userAgent.match(/webOS/i) ||
+  navigator.userAgent.match(/iPhone/i) ||
+  navigator.userAgent.match(/iPad/i) ||
+  navigator.userAgent.match(/iPod/i) ||
+  navigator.userAgent.match(/BlackBerry/i) ||
+  navigator.userAgent.match(/Windows Phone/i)
+) {
+  document.getElementById(
+    'change'
+  ).innerHTML = `<h1 style='font-size: 50px; color: white;'>Must use a device with a keyboard.</h1>`;
 }
